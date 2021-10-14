90893657b085229f4cb33d1c447fa28fcaea381d
Nikhil <nikhil@policyengine.org>
2021-10-14 19:57:36 +0100

    Fix final bug and bump version

--- server/setup.py
+++ server/setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setup(
     name="PolicyEngine-Core",
-    version="0.1.5",
+    version="0.1.6",
     author="PolicyEngine",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     url="https://github.com/policyengine/policyengine-core",
