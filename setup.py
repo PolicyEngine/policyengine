df0396046d245e5a40de2f3e7982498cfd90f57c
Nikhil <nikhil@policyengine.org>
2021-10-22 14:45:27 +0100

    Remove test file and bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setup(
     name="PolicyEngine",
-    version="0.1.7",
+    version="0.2.0",
     author="PolicyEngine",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     url="https://github.com/policyengine/policyengine",
--- test.py
+++
@@ -1,5 +0,0 @@
-from openfisca_uk import Microsimulation
-from openfisca_uk_data import FRS
-
-sim = Microsimulation(dataset=FRS, year=2019)
-sim.calc("employment_income", 2021)
