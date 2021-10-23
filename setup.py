109c561010cbf9c23a6a263fc9666a4215446fee
Nikhil <nikhil@policyengine.org>
2021-10-23 17:38:14 +0100

    Add deps

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     url="https://github.com/policyengine/policyengine",
     install_requires=[
-        # "OpenFisca-UK>=0.4.6,<0.6.0"
+        "OpenFisca-UK>=0.4.6,<0.6.0"
         "plotly",
         "flask",
         "flask_cors",
@@ -21,6 +21,7 @@ setup(
         "pandas",
         "tables",
         "wheel",
+        "rdbl",
     ],
     packages=find_packages(),
 )
