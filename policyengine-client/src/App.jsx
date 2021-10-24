d375b805bbcc322ef3ef607613df763d6d18abbe
Nikhil <nikhil@policyengine.org>
2021-10-24 17:20:59 +0100

    Set API url to relational

--- policyengine-client/src/App.jsx
+++ policyengine-client/src/App.jsx
@@ -15,7 +15,7 @@ export default class App extends React.Component {
 		return (
 			<Router>
 				<Route path="/uk">
-					<PolicyEngineUK analytics={this.props.analytics} api_url="https://policyengine.org/uk/api" />
+					<PolicyEngineUK analytics={this.props.analytics} api_url="/uk/api" />
 				</Route>
 				<Route path="/us">
 					Coming soon!
