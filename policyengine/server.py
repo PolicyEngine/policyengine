363d15ef86b07219cd33dea32e4dd208e9261833
Nikhil <nikhil@policyengine.org>
2021-10-26 17:53:54 +0100

    Cache

--- policyengine/server.py
+++ policyengine/server.py
@@ -16,7 +16,7 @@ from policyengine.countries import UK, PolicyEngineCountry
 
 class PolicyEngine:
     version: str = "0.2.2"
-    cache_bucket_name: str = None
+    cache_bucket_name: str = "uk-policy-engine.appspot.com"
     countries: Tuple[Type[PolicyEngineCountry]] = (UK,)
 
     def _init_countries(self):
