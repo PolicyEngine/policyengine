4beec1a43971621e5e1eb7409571953d30c7d1d7
Max Ghenis <mghenis@gmail.com>
2021-10-28 07:38:53 -0700

    bump version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from pathlib import Path
 
 setup(
     name="PolicyEngine",
-    version="0.2.8",
+    version="0.2.9",
     author="PolicyEngine",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     url="https://github.com/policyengine/policyengine",
     install_requires=[
-        "OpenFisca-UK>=0.5.8,<0.6.0",
+        "OpenFisca-UK>=0.5.9,<0.6.0",
         "plotly",
         "flask",
         "flask_cors",
