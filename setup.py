c78c8628cd490a2ad26b3ec7adea7f102c58f91a
Max Ghenis <mghenis@gmail.com>
2021-10-28 10:10:07 -0700

    Bump versions

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setup(
     name="PolicyEngine",
-    version="0.2.11",
+    version="0.2.12",
     author="PolicyEngine",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     url="https://github.com/policyengine/policyengine",
