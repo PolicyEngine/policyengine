5974a79c3db2b30ba620e518448c20f4cce38ccd
Nikhil <nikhil@policyengine.org>
2021-11-06 15:48:14 +0000

    Fix bug

--- policyengine/server.py
+++ policyengine/server.py
@@ -15,7 +15,7 @@ from policyengine.countries import UK, PolicyEngineCountry
 
 
 class PolicyEngine:
-    version: str = "1.1.0"
+    version: str = "1.1.1"
     cache_bucket_name: str = "uk-policy-engine.appspot.com"
     countries: Tuple[Type[PolicyEngineCountry]] = (UK,)
 
