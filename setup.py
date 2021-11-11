11b10633893ee3702eda532ce22154ac591ee2ef
Nikhil <nikhil@policyengine.org>
2021-11-11 16:46:10 +0000

    Bump openfisca-uk dependency and version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from pathlib import Path
 
 setup(
     name="PolicyEngine",
-    version="1.1.4",
+    version="1.1.5",
     author="PolicyEngine",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     url="https://github.com/policyengine/policyengine",
     install_requires=[
-        "OpenFisca-UK>=0.7.0,<0.8.0",
+        "OpenFisca-UK>=0.7.1,<0.8.0",
         "plotly",
         "flask",
         "flask_cors",
