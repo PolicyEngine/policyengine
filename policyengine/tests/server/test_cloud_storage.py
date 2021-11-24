57f0a7231bb3671661d788ebdb3f66912b0cf695
Nikhil <nikhil@policyengine.org>
2021-11-24 23:27:36 +0000

    Add hosted site tests

---
+++ policyengine/tests/server/test_cloud_storage.py
@@ -0,0 +1,4 @@
+def test_storage():
+    from google.cloud import storage
+
+    storage.Client()
---
+++ policyengine/tests/server/test_server.py
@@ -0,0 +1,4 @@
+def test_server_starts():
+    from policyengine.server import app
+
+    app.run()
