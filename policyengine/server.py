01102cc8dfe72a1e363f1b7afa21c2c7adb6fa13
Nikhil <nikhil@policyengine.org>
2021-11-25 22:49:46 +0000

    Bump version

--- policyengine/server.py
+++ policyengine/server.py
@@ -15,7 +15,7 @@ from policyengine.countries import UK, US, PolicyEngineCountry
 
 
 class PolicyEngine:
-    version: str = "1.2.0"
+    version: str = "1.2.1"
     cache_bucket_name: str = "uk-policy-engine.appspot.com"
     countries: Tuple[Type[PolicyEngineCountry]] = (UK, US)
 
