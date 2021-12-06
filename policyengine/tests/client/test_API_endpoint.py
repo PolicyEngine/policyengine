91a3fa91806b5aba8a1b84e7d6662bf77c9fc365
Nikhil <nikhil@policyengine.org>
2021-12-06 14:20:13 +0000

    Apply formatting

--- policyengine/tests/client/test_API_endpoint.py
+++ policyengine/tests/client/test_API_endpoint.py
@@ -19,4 +19,6 @@ def in_text_files(folder: Path, text: str):
 
 
 def test_localhost_included():
-    assert not in_text_files(REPO / "policyengine-client" / "src", "useLocalServer = true")
+    assert not in_text_files(
+        REPO / "policyengine-client" / "src", "useLocalServer = true"
+    )
