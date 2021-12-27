e9efea4637549fe458c720dfb91eca0e0f15a2a0
Nikhil <nikhil@policyengine.org>
2021-12-27 12:51:15 +0000

    Fix named policy redirects

--- policyengine-client/src/App.jsx
+++ policyengine-client/src/App.jsx
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./common/policyengine.less";
 import { PolicyEngineUK } from "./countries/uk";
 import { PolicyEngineUS } from "./countries/us";
+import { REDIRECTS } from "./countries/uk/data/namedPolicies";
 
 export default class App extends React.Component {
 	constructor(props) {
@@ -15,9 +16,11 @@ export default class App extends React.Component {
 		const useLocalServer = false;
 		let apiUrl = `${window.location.protocol}//${window.location.hostname}`;
 		apiUrl = useLocalServer ? "http://localhost:5000" : apiUrl;
+		const namedPolicies  = Object.keys(REDIRECTS).map(url => <Route exact path={`/uk${url}`}><Redirect to={`/uk/population-impact?${REDIRECTS[url]}`} /></Route>);
 		return (
 			<Router>
 				<Route path="/uk">
+					{namedPolicies}
 					<PolicyEngineUK analytics={this.props.analytics} api_url={`${apiUrl}/uk/api`} />
 				</Route>
 				<Route path="/us">
