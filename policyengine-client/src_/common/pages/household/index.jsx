923182dba0d2e4974bc584d85b9eecc089fe5d36
Nikhil <nikhil@policyengine.org>
2022-01-07 12:58:47 +0000

    Remove old code

--- policyengine-client/src_/common/pages/household/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class Household extends React.Component {
-    render() {
-        return "Household"
-    }
-}
-
-export default Household;
--- policyengine-client/src_/countries/uk/index.jsx
+++
@@ -1,1 +0,0 @@
-export { PolicyEngineUK } from "./app";
