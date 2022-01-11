fc62b82a35230b735e1046406a133a5808f29215
Max Ghenis <mghenis@gmail.com>
2022-01-11 14:20:31 -0800

    Update footer copyright year. Fixes #353

--- policyengine-client/src/common/footer.jsx
+++ policyengine-client/src/common/footer.jsx
@@ -5,20 +5,20 @@ import { Divider } from "antd";
 
 export function Footer(props) {
 	return (
-		<div style={{paddingBottom: 25}}>
+		<div style={{ paddingBottom: 25 }}>
 			<BackTop />
-			<Divider style={{marginTop: 50}} />
+			<Divider style={{ marginTop: 50 }} />
 			<div className="d-none d-lg-block">
 				<div className="d-flex justify-content-center">
-					<p style={{textAlign: "center"}}><a href="https://policyengine.org">PolicyEngine © 2021</a> | <a href={`/${props.country}/faq`}>FAQ</a> | <a href="https://blog.policyengine.org">Blog</a> | <a href="https://zej8fnylwn9.typeform.com/to/XFFu15Xq">Feedback</a> | <a href="https://opencollective.com/psl">Donate</a></p>
+					<p style={{ textAlign: "center" }}><a href="https://policyengine.org">PolicyEngine © 2022</a> | <a href={`/${props.country}/faq`}>FAQ</a> | <a href="https://blog.policyengine.org">Blog</a> | <a href="https://zej8fnylwn9.typeform.com/to/XFFu15Xq">Feedback</a> | <a href="https://opencollective.com/psl">Donate</a></p>
 				</div>
 			</div>
 			<div className="d-flex d-lg-none justify-content-center">
-				<SocialLinks color="black"/>
+				<SocialLinks color="black" />
 			</div>
 			<div className="d-block d-lg-none">
-				<p style={{textAlign: "center"}}><a href="https://policyengine.org">PolicyEngine © 2021</a> | <a href={`/${props.country}/faq`}>FAQ</a> | <a href="https://blog.policyengine.org">Blog</a></p>
-				<p style={{textAlign: "center"}}><a href="https://zej8fnylwn9.typeform.com/to/XFFu15Xq">Share your feedback</a> | <a href="https://opencollective.com/psl">Donate</a></p>
+				<p style={{ textAlign: "center" }}><a href="https://policyengine.org">PolicyEngine © 2022</a> | <a href={`/${props.country}/faq`}>FAQ</a> | <a href="https://blog.policyengine.org">Blog</a></p>
+				<p style={{ textAlign: "center" }}><a href="https://zej8fnylwn9.typeform.com/to/XFFu15Xq">Share your feedback</a> | <a href="https://opencollective.com/psl">Donate</a></p>
 			</div>
 		</div>
 	)
