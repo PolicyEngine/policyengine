61bc94cb12215624a066950f574cd6320d2da553
Max Ghenis <mghenis@gmail.com>
2022-01-11 15:16:57 -0800

    Add about to footer

--- policyengine-client/src/policyengine/footer/footer.jsx
+++ policyengine-client/src/policyengine/footer/footer.jsx
@@ -12,14 +12,14 @@ export default function Footer() {
 			<Divider style={{ marginTop: 50 }} />
 			<div className="d-none d-lg-block">
 				<div className="d-flex justify-content-center">
-					<p style={{ textAlign: "center" }}><a href="https://policyengine.org">PolicyEngine © 2022</a> | <a href={`/${country.name}/faq`}>FAQ</a> | <a href="https://blog.policyengine.org">Blog</a> | <a href="https://zej8fnylwn9.typeform.com/to/XFFu15Xq">Feedback</a> | <a href="https://opencollective.com/psl">Donate</a></p>
+					<p style={{ textAlign: "center" }}><a href="https://policyengine.org">PolicyEngine © 2022</a> | <a href={`/${country.name}/about`}>About</a> | <a href={`/${country.name}/faq`}>FAQ</a> | <a href="https://blog.policyengine.org">Blog</a> | <a href="https://zej8fnylwn9.typeform.com/to/XFFu15Xq">Feedback</a> | <a href="https://opencollective.com/psl">Donate</a></p>
 				</div>
 			</div>
 			<div className="d-flex d-lg-none justify-content-center">
 				<SocialLinks color="black" />
 			</div>
 			<div className="d-block d-lg-none">
-				<p style={{ textAlign: "center" }}><a href="https://policyengine.org">PolicyEngine © 2022</a> | <a href={`/${country.name}/faq`}>FAQ</a> | <a href="https://blog.policyengine.org">Blog</a></p>
+				<p style={{ textAlign: "center" }}><a href="https://policyengine.org">PolicyEngine © 2022</a> | <a href={`/${country.name}/about`}>About</a> | <a href={`/${country.name}/faq`}>FAQ</a> | <a href="https://blog.policyengine.org">Blog</a></p>
 				<p style={{ textAlign: "center" }}><a href="https://zej8fnylwn9.typeform.com/to/XFFu15Xq">Share your feedback</a> | <a href="https://opencollective.com/psl">Donate</a></p>
 			</div>
 		</div>
