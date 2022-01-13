0f94cefea6ffbea29412b448eabe1e0b1c21154e
Nikhil <nikhil@policyengine.org>
2022-01-13 13:07:37 +0000

    Bump openfisca-uk version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     url="https://github.com/policyengine/policyengine",
     install_requires=[
-        "OpenFisca-UK==0.10.1",
+        "OpenFisca-UK==0.10.3",
         "OpenFisca-US==0.1.5",
         "OpenFisca-Tools>=0.1.7",
         "plotly",
