8fa1e221f1f0aa3c24e1a5c0cb592754d9e2eb2a
Nikhil <nikhil@policyengine.org>
2022-01-14 11:34:22 +0000

    Add metadata

--- policyengine-client/src/countries/us/us.jsx
+++ policyengine-client/src/countries/us/us.jsx
@@ -12,6 +12,15 @@ const childNamer = {
     5: "Your fifth child",
 }
 
+function validatePolicy(policy, defaultPolicy) {
+    if(defaultPolicy) {
+        for(let parameter in policy) {
+            policy[parameter].defaultValue = defaultPolicy[parameter].value;
+        }
+    }
+    return {policy: policy, policyValid: true};
+}
+
 export class US extends Country {
     constructor() {
         super();
@@ -28,7 +37,7 @@ export class US extends Country {
     properName = "US"
     beta = false
     // Pages to show
-    showPolicy = false
+    showPolicy = true
     showPopulationImpact = false
     showHousehold = true
     showFAQ = true
@@ -37,10 +46,22 @@ export class US extends Country {
         "/ubi-labs/resilience-ubi": "WA_adult_UBI_age=16&adult_UBI=184&child_UBI=92&senior_UBI=184&abolish_CB=1&abolish_CTC=1&abolish_ESA_income=1&abolish_IS=1&abolish_JSA_income=1&abolish_PC=1&abolish_SP=1&abolish_UC_carer=1&abolish_UC_child=1&abolish_UC_childcare=1&abolish_UC_standard=1&abolish_WTC=1&personal_allowance=0&higher_threshold=50000&add_rate=60&basic_rate=35&higher_rate=55&NI_add_rate=10&NI_class_4_add_rate=10&NI_class_4_main_rate=10&NI_main_rate=10",
         "/ubi-labs/covid-dividend": "child_UBI=46&adult_UBI=92&senior_UBI=46&WA_adult_UBI_age=16"
     }
+    validatePolicy = validatePolicy;
     // Policy page metadata
-    parameterHierarchy = {}
-    defaultOpenParameterGroups = []
-    defaultSelectedParameterGroup = null
+    parameterHierarchy = {
+        "SNAP": {
+            "Eligibility": [
+                "snap_gross_income_limit_standard",
+                "snap_gross_income_limit_elderly_disabled",
+                "snap_net_income_limit_standard",
+            ],
+            "Deductions": [
+                "snap_earnings_deduction",
+            ]
+        }
+    }
+    defaultOpenParameterGroups = ["/SNAP"]
+    defaultSelectedParameterGroup = "/SNAP/Eligibility"
     organisations = {}
     // OpenFisca data
     parameters = null
@@ -77,51 +98,39 @@ export class US extends Country {
     inputVariables = [
         "age",
         "state_code",
+        "market_income",
     ]
     outputVariables = [
+        "spm_unit_net_income",
+        "snap_max_benefit",
+        "snap_expected_contribution",
         "snap",
-        "snap_deductions",
-        "snap_gross_income",
-        "snap_net_income",
-        "snap_standard_deduction",
-        "snap_earnings_deduction",
-        "snap_dependent_care_deduction",
-        "snap_child_support_deduction",
-        "snap_medical_expense_deduction",
-        "snap_shelter_deduction",
     ]
     inputVariableHierarchy = {
         "General": [],
-        "Demographic": {
-            "Personal" : [
-                "age",
-            ],
-            "Household": [
-                "state_code",
-            ]
-        },
+        "Personal" : [
+            "age",
+            "market_income",
+        ],
+        "Household": [
+            "state_code",
+        ]
     }
-    defaultOpenVariableGroups = [
-        "/Demographic",
-    ]
+    defaultOpenVariableGroups = []
     defaultSelectedVariableGroup = "/General"
     outputVariableHierarchy = {
-        "snap_net_income": {
+        "spm_unit_net_income": {
             "add": [
-                "snap_gross_income",
+                "snap",
+            ],
+            "subtract": []
+        },
+        "snap": {
+            "add": [
+                "snap_max_benefit",
             ],
             "subtract": [
-                "snap_deductions",
-            ]
-        },
-        "snap_deductions": {
-            "add": [
-                "snap_standard_deduction",
-                "snap_earnings_deduction",
-                "snap_dependent_care_deduction",
-                "snap_child_support_deduction",
-                "snap_medical_expense_deduction",
-                "snap_shelter_deduction",
+                "snap_expected_contribution",
             ]
         }
     }
