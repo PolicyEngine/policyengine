d6b6896d3369c19bf1bc215b91c908c1f6031570
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-01-20 09:03:49 +0000

    Use equivalised income in inequality charts

--- policyengine/impact/population/charts.py
+++ policyengine/impact/population/charts.py
@@ -376,37 +376,31 @@ def inequality_chart(
     reformed: Microsimulation,
     config: Type[PolicyEngineResultsConfig],
 ) -> dict:
-    income = baseline.calc(
-        config.household_net_income_variable, map_to="person"
-    )
-    reform_income = reformed.calc(
-        config.household_net_income_variable, map_to="person"
-    )
     equiv_income = baseline.calc(
         config.equiv_household_net_income_variable, map_to="person"
     )
     reform_equiv_income = reformed.calc(
         config.equiv_household_net_income_variable, map_to="person"
     )
-    baseline_gini = income.gini()
-    reform_gini = reform_income.gini()
+    baseline_gini = equiv_income.gini()
+    reform_gini = reform_equiv_income.gini()
     gini_change = reform_gini / baseline_gini - 1
     baseline_top_ten_pct_share = (
-        income[equiv_income.decile_rank() == 10].sum() / income.sum()
+        equiv_income[equiv_income.decile_rank() == 10].sum() / equiv_income.sum()
     )
     reform_top_ten_pct_share = (
-        reform_income[reform_equiv_income.decile_rank() == 10].sum()
-        / reform_income.sum()
+        reform_equiv_income[reform_equiv_income.decile_rank() == 10].sum()
+        / reform_equiv_income.sum()
     )
     top_ten_pct_share_change = (
         reform_top_ten_pct_share / baseline_top_ten_pct_share - 1
     )
     baseline_top_one_pct_share = (
-        income[equiv_income.percentile_rank() == 100].sum() / income.sum()
+        equiv_income[equiv_income.percentile_rank() == 100].sum() / equiv_income.sum()
     )
     reform_top_one_pct_share = (
-        reform_income[reform_equiv_income.percentile_rank() == 100].sum()
-        / reform_income.sum()
+        reform_equiv_income[reform_equiv_income.percentile_rank() == 100].sum()
+        / reform_equiv_income.sum()
     )
     top_one_pct_share_change = (
         reform_top_one_pct_share / baseline_top_one_pct_share - 1
