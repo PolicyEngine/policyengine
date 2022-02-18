1b0c3c4a8e99164ba61051e819a55111cde944fc
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-02-18 17:35:52 +0000

    Add images

--- policyengine-client/src/landing.jsx
+++ policyengine-client/src/landing.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { Header } from "./policyengine/header";
 import { PolicyEngineWrapper } from "./policyengine/layout/general";
 import { Row, Col, Container } from "react-bootstrap";
-import { Card } from "antd";
-import Parliament from "./images/parliament.jpg";
+import { Card, Divider } from "antd";
 import moment from "moment";
+
+import UKFadedBlue from "./images/uk_faded_blue.png";
+import USFadedBlue from "./images/us_faded_blue.png";
 
 export default function LandingPage() {
     return (
@@ -22,21 +24,32 @@ function LandingPageContent() {
         <Row>
             <Col md={6}>
                 <Card 
-                    title="PolicyEngine UK" 
+                    style={{marginTop: 10}} 
+                    hoverable
                     bordered={false}
-                    cover={<img alt="UK" src={UK} />}
+                    cover={<img alt="UK" src={UKFadedBlue} />}
+                    onClick={() => window.location.href = "/uk"}
                 >
                     Explore the impact of tax-benefit reforms on UK households.
                 </Card>
             </Col>
             <Col md={6}>
                 <Card 
-                    title="PolicyEngine US" 
+                    style={{marginTop: 10}} 
+                    hoverable
                     bordered={false}
+                    cover={<img alt="US" src={USFadedBlue} />}
+                    onClick={() => window.location.href = "/us"}
                 >
                     Explore the impact of tax-benefit reforms on US households.
                 </Card>
             </Col>
+        </Row>
+        <Row style={{marginTop: 10, marginBottom: 10}}>
+            <h4>Commentary</h4>
+            <p>Analyses of policy reforms by the PolicyEngine team.</p>
+        </Row>
+        <Row>
             <MediumFeed />
         </Row>
     </>
@@ -63,9 +76,12 @@ class MediumFeed extends React.Component {
         //return JSON.stringify(this.state.feed.items.map(item => item.title));
         return this.state.feed.items.map(post => 
             <Col md={3}>
-                <Card title={<p>{post.title}</p>} bordered={false} cover={
-                    <img src={post.thumbnail} />
-                }>
+                <Card hoverable style={{marginTop: 10}} bordered={false} cover={
+                    <img style={{minHeight: 200, objectFit: "cover"}} alt={post.title + " cover image"} src={post.thumbnail} />
+                }
+                    onClick={() => window.open(post.link, "_blank")}
+                >
+                    <h6>{post.title}</h6>
                 </Card>
             </Col>
         )
