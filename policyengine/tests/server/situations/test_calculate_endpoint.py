bfd999d2c05990071703825dfee2146cfc77458d
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-03-24 12:58:28 +0000

    Add unit test

---
+++ policyengine/tests/server/situations/test_calculate_endpoint.py
@@ -0,0 +1,26 @@
+import requests
+
+
+def test_basic_usage():
+    from policyengine.server import app
+
+    with app.test_client() as client:
+        response = client.post(
+            "/us/api/calculate",
+            json={
+                "household": {
+                    "people": {"person": {}},
+                    "spm_units": {
+                        "spm_unit": {
+                            "members": ["person"],
+                            "snap": {"2022": None},
+                        },
+                    },
+                }
+            },
+        )
+
+        snap = response.json()["spm_units"]["spm_unit"]["snap"]
+
+        assert not isinstance(snap, list)
+        assert snap["2022"] > 0
