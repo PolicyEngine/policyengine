7c21705d34d337d9b0cccec143ee0a9f496517b8
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-03-29 11:25:44 +0100

    Reduce plotly bundle size

--- policyengine-client/src/countries/country.jsx
+++ policyengine-client/src/countries/country.jsx
@@ -107,7 +107,7 @@ export default class Country {
         });
     }
 
-    useLocalServer = false;
+    useLocalServer = true;
     usePolicyEngineOrgServer = false;
 
     waitingOnPopulationImpact = false;
