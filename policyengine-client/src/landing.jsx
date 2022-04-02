1007b4f6cede594f250c8c09497c999acf453b82
Max Ghenis <mghenis@gmail.com>
2022-04-02 12:24:15 -0700

    Adjust frontpage layout fixes #616

--- policyengine-client/src/landing.jsx
+++ policyengine-client/src/landing.jsx
@@ -37,23 +37,21 @@ function UsageBox(props) {
 
     // Displays the title anchored to the left (and big) and the description anchored to the right (and small)
 
-    return <Row style={{ marginTop: 20, marginBottom: 20 }}>
-        <Col xs={12} md={6}>
-            <h3 style={{ marginBottom: 0 }}>{props.title}</h3>
-        </Col>
-        <Col xs={12} md={6}>
-            <h6 style={{ marginTop: 0 }}>{props.description}</h6>
-        </Col>
+    return <center><Row style={{ marginTop: 20, marginBottom: 20 }}>
+        <h3 style={{ marginBottom: 0 }}>{props.title}</h3>
+        <br /><br />
+        <h5 style={{ marginTop: 0 }}>{props.description}</h5>
     </Row>
+    </center>
 
 
 }
 
 function UsageExplanations(props) {
     return <>
-        <UsageBox title="Citizens" description="Check your eligibility for government benefits and programs. Simulate how a change in requirements could affect your household." />
+        <UsageBox title="The public" description="Check your eligibility for government benefits and programs. Simulate how a change in requirements could affect your household." />
         <UsageBox title="Think tanks" description="Simulate tax-benefit reforms on the UK economy and individual households to better understand and recommend policy changes." />
-        <UsageBox title="Parties and campaigns" description="Estimate the economic outcomes of your policy proposals. Produce evidence-based talking points and refute speculative criticism of your platform." />
+        <UsageBox title="Parties and campaigns" description="Estimate the results of your policy proposals. Produce evidence-based talking points and refute speculative criticism of your platform." />
         <UsageBox title="Developers" description={<>Allow your users to check their benefits eligibility with <a href="https://docs.google.com/document/d/1y-kRDOssYyRwEVTsntqGxoBtvFS4HKMQD-U0Ga9YzJE/preview">our API</a>. <a href="https://github.com/PolicyEngine">Contribute to our work on GitHub.</a></>} />
     </>
 }
@@ -62,35 +60,33 @@ function LandingPageContent() {
     const in_uk = window.navigator.language === "en-GB";
     const in_us = window.navigator.language === "en-US";
     const outside_uk_us = !in_uk && !in_us;
-    const ukIcon = <Col md={6} style={{ marginBottom: 15 }} key={"ukIcon"}>
-        <img onClick={() => window.location.href = "/uk/policy"} className="img-fluid" style={{ borderRadius: 35, cursor: "pointer" }} alt="UK" src={UKFadedBlue} />
-    </Col>;
-    const usIcon = <Col md={6} style={{ marginBottom: 15 }} key={"usIcon"}>
-        <img onClick={() => window.location.href = "/us/household"} className="img-fluid" style={{ borderRadius: 35, cursor: "pointer" }} alt="US" src={USFadedBlue} />
-    </Col>;
+    const ukIcon = <img onClick={() => window.location.href = "/uk/policy"} className="img-fluid" style={{ borderRadius: 35, cursor: "pointer" }} alt="UK" src={UKFadedBlue} />;
+    const usIcon = <img onClick={() => window.location.href = "/us/household"} className="img-fluid" style={{ borderRadius: 35, cursor: "pointer" }} alt="US" src={USFadedBlue} />;
+    const ukLink = <h5><a href="/uk">Or enter PolicyEngine UK</a></h5>
+    const usLink = <h5><a href="/us">Or enter PolicyEngine US (beta)</a></h5>
     return <>
-        <Row>
-            <Col lg={10} style={{ paddingTop: 50 }}>
-                <h4><b>PolicyEngine</b> empowers people to understand and change public policy. </h4><br /><h4>Our app lets anyone imagine reforms to the tax and benefit system and see the impact on society and their own household.</h4>
-            </Col>
-            <Col lg={2}></Col>
-        </Row>
-        <Subheader><a href={(in_uk ? "/uk/policy" : (in_us ? "/us/household" : "/uk/policy"))}>Use the app→</a></Subheader>
-        <Row>
-            {
-                outside_uk_us ?
-                    [ukIcon, usIcon] :
-                    in_uk ?
+        <center>
+            <br /><br /><br />
+            <Row>
+                <h1>Compute the impact of public policy</h1><br /><h4>Estimate your taxes and benefits<br />Imagine reforms to the tax and benefit system<br />Calculate the impact on society and your own household</h4>
+            </Row>
+            <Subheader><a href={(in_uk ? "/uk/policy" : (in_us ? "/us/household" : "/uk/policy"))}>Use the free app→</a></Subheader>
+            <Row>
+                {
+                    outside_uk_us ?
                         [ukIcon, usIcon] :
-                        [usIcon, ukIcon]
-            }
-        </Row>
-        <Subheader>Who we help</Subheader>
-        <UsageExplanations />
-        <Subheader subtitle={<>We're currently seeking funding partners, volunteer developers and policy analysts to expand our work and its impact. Is that you? <a href="mailto:hello@policyengine.org">Get in touch.</a></>}></Subheader>
-        <Subheader>Blog</Subheader>
-        <MediumFeed />
-        <Footer />
+                        in_uk ?
+                            [ukIcon, usLink] :
+                            [usIcon, ukLink]
+                }
+            </Row>
+            <Subheader>Who we help</Subheader>
+            <UsageExplanations />
+            <Subheader subtitle={<>We're currently seeking funding partners, volunteer developers and policy analysts to expand our work and its impact. Is that you? <a href="mailto:hello@policyengine.org">Get in touch.</a></>}></Subheader>
+            <Subheader>Blog</Subheader>
+            <MediumFeed />
+            <Footer />
+        </center>
     </>
 }
 
