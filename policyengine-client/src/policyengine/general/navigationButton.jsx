d5ca453e125801f5c2b839378647f43abf0d2804
Max Ghenis <mghenis@gmail.com>
2022-04-12 11:58:46 -0700

    Try to block the navigation buttons

--- policyengine-client/src/policyengine/general/navigationButton.jsx
+++ policyengine-client/src/policyengine/general/navigationButton.jsx
@@ -5,18 +5,19 @@ import { CountryContext } from "../../countries";
 import { policyToURL } from "../tools/url";
 
 export default function SimulateButton(props) {
-    const country = useContext(CountryContext);
+	const country = useContext(CountryContext);
 	const url = policyToURL(`/${country.name}/${props.target}`, country.policy);
-	let button = <Button 
-			disabled={props.disabled} 
-			type={props.primary ? "primary" : null} 
-			onClick={props.onClick}>{props.text}
-		</Button>;
-	if(props.target) {
+	let button = <Button
+		disabled={props.disabled}
+		type={props.primary ? "primary" : null}
+		onClick={props.onClick}>{props.text}
+		block
+	</Button>;
+	if (props.target) {
 		button = <Link to={url}>{button}</Link>;
 	}
 	return (
-		<div style={{marginBottom: 20}}>
+		<div style={{ marginBottom: 20 }}>
 			{button}
 		</div>
 	);
