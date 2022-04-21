e52a3ace29321eff0641a3287ed9b72473cd4d67
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-04-21 21:13:03 +0100

    Add variables

--- policyengine/countries/us/__init__.py
+++ policyengine/countries/us/__init__.py
@@ -1,8 +1,9 @@
 from openfisca_us import CountryTaxBenefitSystem
 from openfisca_us.entities import *
+from policyengine.countries.us.default_reform import create_default_reform
 from policyengine.utils.general import PolicyEngineResultsConfig
 from policyengine.countries.country import PolicyEngineCountry
-from openfisca_us import IndividualSim
+from openfisca_us import IndividualSim, Microsimulation, CPS
 
 
 class USResultsConfig(PolicyEngineResultsConfig):
@@ -12,13 +13,25 @@ class USResultsConfig(PolicyEngineResultsConfig):
     employment_income_variable: str = "employment_income"
     self_employment_income_variable: str = "self_employment_income"
     total_income_variable: str = "spm_unit_market_income"
+    in_poverty_variable = "spm_unit_is_in_spm_poverty"
+    in_deep_poverty_variable = "spm_unit_is_in_deep_spm_poverty"
+    household_wealth_variable = "spm_unit_net_income"  # Placeholder
+    equiv_household_net_income_variable = "spm_unit_oecd_equiv_net_income"
+    child_variable = "is_child"
+    working_age_variable = "is_wa_adult"
+    senior_variable = "is_senior"
+    person_variable = "people"
+    earnings_variable = "employment_income"
+    household_entity = "spm_unit"
     currency = "$"
 
 
 class US(PolicyEngineCountry):
     name = "us"
     system = CountryTaxBenefitSystem
-    calculate_only = True
-    household_page_only = True
     IndividualSim = IndividualSim
     results_config = USResultsConfig
+    Microsimulation = Microsimulation
+    default_dataset = CPS
+    default_dataset_year = 2020
+    default_reform = create_default_reform()
