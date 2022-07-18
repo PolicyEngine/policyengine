584e77ef18e8b5d7f58ca9c3c2b28afe5c5c4f40
Max Ghenis <mghenis@gmail.com>
2022-07-18 12:33:42 -0700

    undo useLocalServer

--- policyengine-client/src/countries/country.jsx
+++ policyengine-client/src/countries/country.jsx
@@ -114,7 +114,7 @@ export default class Country {
         });
     }
 
-    useLocalServer = true;
+    useLocalServer = false;
     usePolicyEngineOrgServer = false;
 
     waitingOnPopulationImpact = false;
