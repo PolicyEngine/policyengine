c667af8bc11c552ff68a73a76894596d3e263cd8
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-08-24 11:25:49 -0600

    Remove sociallinks from bottom so only render once

--- policyengine-client/src/fof.jsx
+++ policyengine-client/src/fof.jsx
@@ -1,24 +1,11 @@
 import React from "react";
-import SocialLinks from "./policyengine/header/socialLinks";
-
+import { Header } from "./policyengine/header";
 
 export default function FOF() {
     return (
-        <>
+        <div style={{ height: "100%" }}>
+            <Header noTabs />
             <p style={{ marginTop: 180, fontSize: "2em", textAlign: "center" }}>This page does not exist, please navigate home.</p>
-            <div style={{
-            position: "fixed",
-            height: "4.2em",
-            width: "100%",
-            bottom: 0,
-            backgroundColor: "white",
-            borderTop: "1px solid",
-            borderColor: "#EEE",
-            paddingTop: 20,
-            paddingBottom: 20
-            }}>
-                <SocialLinks color={"black"}/>
-            </div>
-        </>
+        </div>
     )
 }
