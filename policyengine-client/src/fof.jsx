a306bed1285c9256badc4ab07782868c7234bf0f
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-08-24 11:11:40 -0600

    Fix indentation

--- policyengine-client/src/fof.jsx
+++ policyengine-client/src/fof.jsx
@@ -5,20 +5,20 @@ import SocialLinks from "./policyengine/header/socialLinks";
 export default function FOF() {
     return (
         <>
-        <p style={{ marginTop: 180, fontSize: "2em", textAlign: "center" }}>This page does not exist, please navigate home.</p>
-        <div style={{
-        position: "fixed",
-        height: "4.2em",
-        width: "100%",
-        bottom: 0,
-        backgroundColor: "white",
-        borderTop: "1px solid",
-        borderColor: "#EEE",
-        paddingTop: 20,
-        paddingBottom: 20
-        }}>
-            <SocialLinks color={"black"}/>
-        </div>
+            <p style={{ marginTop: 180, fontSize: "2em", textAlign: "center" }}>This page does not exist, please navigate home.</p>
+            <div style={{
+            position: "fixed",
+            height: "4.2em",
+            width: "100%",
+            bottom: 0,
+            backgroundColor: "white",
+            borderTop: "1px solid",
+            borderColor: "#EEE",
+            paddingTop: 20,
+            paddingBottom: 20
+            }}>
+                <SocialLinks color={"black"}/>
+            </div>
         </>
     )
 }
