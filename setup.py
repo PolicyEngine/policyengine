5c51572d9095dac4b6601fe173a3d8b8fc3d4e26
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-09-05 18:05:05 +0100

    Bump openfisca-us

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         "OpenFisca-Core",
         "OpenFisca-Tools>=0.13.3",
         "OpenFisca-UK==0.29.0",
-        "OpenFisca-US==0.135.1",
+        "OpenFisca-US==0.147.0",
         "pandas",
         "plotly",
         "pytest",
