65731c0669ddf7bf14e00d84d2b3d64c1d901ab2
Max Ghenis <mghenis@gmail.com>
2022-09-13 12:44:03 -0700

    Add US basic income taxability Fixes #1006

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         "OpenFisca-Core",
         "OpenFisca-Tools>=0.13.3",
         "OpenFisca-UK==0.30.1",
-        "OpenFisca-US==0.148.0",
+        "OpenFisca-US==0.155.0",
         "pandas",
         "plotly",
         "pytest",
