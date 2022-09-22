6a34c2953d0f007e94183db57f71201e9e3d90d8
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-09-22 10:04:36 +0100

    Re-implement AutoUBI

--- policyengine-client/src/countries/uk/components/autoUBI.jsx
+++ policyengine-client/src/countries/uk/components/autoUBI.jsx
@@ -3,52 +3,75 @@ import { Button, Divider, Alert, Spin, message } from "antd";
 import { LoadingOutlined } from "@ant-design/icons";
 import { CountryContext } from "../../country";
 
-export default class AutoUBI extends React.Component {
-	static contextType = CountryContext;
-	constructor(props) {
-		super(props);
-		this.state = {waiting: false, amount: 0};
-		this.applyAutoUBI = this.applyAutoUBI.bind(this);
+function computeUBI(countryContext, callBack) {
+	// This function sends an API call to the AutoUBI endpoint,
+	// passing in the current policy as a JSON payload. It then
+	// checks the results every 2 seconds until it gets a non-loading
+	// response. Upon completion, it calls the callback function with
+	// the UBI amount.
+	const submission = countryContext.getPolicyJSONPayload();
+	let url = new URL(countryContext.apiURL + "/auto-ubi");
+	url.search = new URLSearchParams(submission).toString();
+	const roundAndWeeklyise = (x) => Math.round(x / 52 * 100) / 100;
+	return fetch(url)
+		.then((response) => response.json())
+		.then((data) => {
+			if(data.status === "completed") {
+				callBack(roundAndWeeklyise(data.UBI));
+			} else {
+				let checker = setInterval(() => {
+					fetch(url).then(res => res.json()).then(data => {
+						if (data.status === "completed") {
+							clearInterval(checker);
+							callBack(roundAndWeeklyise(data.UBI))
+						}
+					})
+				}, 1000 * 2);
+			}
+		});
+}
+
+function updatePolicyWithUBI(countryContext, UBI) {
+	// This function updates the policy with the UBI amount.
+	let newPolicy = countryContext.policy;
+	newPolicy.child_bi.value = UBI;
+	newPolicy.adult_bi.value = UBI;
+	newPolicy.senior_bi.value = UBI;
+	countryContext.updateEntirePolicy(newPolicy);
+}
+
+export default function AutoUBI(props) {
+	const [waiting, setWaiting] = React.useState(false);
+	const [ubiAmount, setUBIAmount] = React.useState(0);
+	const country = React.useContext(CountryContext);
+	let loadingMessageIfNeeded;
+
+	if(waiting) {
+		loadingMessageIfNeeded = <Alert style={{marginTop: 10}} message={
+			<>This reform would fund a UBI of £<Spin indicator={<LoadingOutlined />}/>/week</>
+		} />;
+	} else if(ubiAmount > 0) {
+		loadingMessageIfNeeded = <Alert style={{marginTop: 10}} message={
+			<>This reform would fund a UBI of £{ubiAmount}/week</>
+		} /> ;
 	}
 
-	applyAutoUBI() {
-		const submission = this.context.getPolicyJSONPayload();
-		let url = new URL(this.context.apiURL + "/auto-ubi");
-		url.search = new URLSearchParams(submission).toString();
-		this.setState({waiting: true}, () => {
-			fetch(url)
-				.then((res) => {
-					if (res.ok) {
-						return res.json();
-					} else {
-						throw res;
-					}
-				}).then((json) => {
-					const amount = Math.round(json.UBI / 52 * 100) / 100;
-					this.setState({waiting: false, amount: amount});
-					this.context.updatePolicy("child_bi", this.context.policy["child_bi"].value + amount);
-					this.context.updatePolicy("adult_bi", this.context.policy["adult_bi"].value + amount);
-					this.context.updatePolicy("senior_bi", this.context.policy["senior_bi"].value + amount);
-				}).catch(e => {
-					message.error("Couldn't apply AutoUBI - something went wrong." + e.toString());
-					this.setState({waiting: false});
-				});
+	const onButtonClick = () => {
+		setWaiting(true);
+		computeUBI(country, (amount) => {
+			updatePolicyWithUBI(country, amount);
+			setUBIAmount(amount);
+			setWaiting(false);
 		});
-	}
-	
-	render() {
-		let result;
-		if(this.state.waiting) {
-			result = <Alert style={{marginTop: 10}} message={<>This reform would fund a UBI of £<Spin indicator={<LoadingOutlined />}/>/week</>} />;
-		} else if(this.state.amount) {
-			result = <Alert style={{marginTop: 10}} message={<>This reform would fund a UBI of £{this.state.amount}/week</>} /> ;
-		}
-		return (
-			<>
-				<Divider>AutoUBI</Divider>
-				<Button onClick={this.applyAutoUBI}>Direct surplus revenue into UBI</Button>
-				{result}
-			</>
-		);
-	}
+	};
+
+	return (
+		<>
+			<Divider>AutoUBI</Divider>
+			<Button onClick={onButtonClick}>
+				Direct surplus revenue into UBI
+			</Button>
+			{loadingMessageIfNeeded}
+		</>
+	);
 }
